48cfaf95f5a70c4c6026e48b65cee4051daabf83
Don-Stevenson <dsteven1@gmail.com>
2020-04-02 21:16:19 -0400

    add start and update functions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 // variables
@@ -15,8 +15,43 @@ class Player {
     this.y = y;
     this.width = width;
     this.colour = colour;
-    this.dy= 0
+    this.dy = 0;
     this.jumpForce = 15;
-    this.originalHeight = h
+    this.originalHeight = h;
+  }
+
+  Draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.colour;
+    ctx.fillRect = (this.x, this.y, this.width, this.height);
+    ctx.closePath();
   }
 }
+
+function Start() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  ctx.font = "20px sans-serif";
+
+  gameSpeed = 3;
+  gravity = 1;
+
+  score = 0;
+  highScore = 0;
+
+  player = new Player(25, canvas.height - 150, 50, 50, "#FF5858");
+  player.Draw();
+
+  requestAnimationFrame(Update)
+}
+
+function Update() {
+    requestAnimationFrame(Update)
+    ctx.clearReact(0,0, canvas.width, canvas.height)
+
+    player.Draw()
+    player.x++
+}
+
+Start();
